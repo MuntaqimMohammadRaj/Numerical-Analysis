d2c8a332cf086e493e0f8edba491879727235e7f
MUNTAQIM <34061320+MuntaqimMohammadRaj@users.noreply.github.com>
2018-03-17 20:11:48 +0600

    Add files via upload

---
+++ RegularFalsi/src/regularfalsi/RegularFalsi.java
@@ -0,0 +1,87 @@
+package regularfalsi;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RegularFalsi {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> list = new ArrayList<Double>();
+        double a, b, c,prev,root;
+
+        System.out.println("Enter the values of a & b: ");
+        
+            while (true) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if (f(a) * f(b) < 0) {
+                c = formater((a*f(b))-(b*f(a))/(f(b)-f(a)));
+                prev=c;
+                System.out.println("The value of Root: " + c);
+                break ;
+            } else {
+                System.out.println("Enter more Values for a & b:  ");
+            }
+
+        }
+           /*
+            while(true)
+            {
+                  if (f(a) * f(c) <= 0) {
+
+                b = c;
+
+            } else {
+                a = c;
+
+            }
+            c = formater((a + b) / 2);
+           System.out.println("The value of Root: " + c);
+            
+            if(Math.abs(prev-c)<=0.01){
+                root=c;
+                break;
+            }
+            else
+            {
+                prev=c;
+            }
+            }
+            */
+
+        while (true) {
+            if (f(a) * f(c) <= 0) {
+
+                b = c;
+
+            } else {
+                a = c;
+
+            }
+           c = formater(((a*f(b))-(b*f(a)))/(f(b)-f(a)));
+
+            if (list.contains(c)) {
+                break;
+            } else {
+                list.add(c);
+                System.out.println("the value of Root: " + c);
+            }
+        }
+        System.out.println("The Root is: " + c);
+
+    }
+
+    public static double f(double x) {
+        return 3 * x - Math.cos(x) - 1;
+        //return x*x-3;
+    }
+
+    public static double formater(double n) {
+        DecimalFormat df = new DecimalFormat("#.####");
+        n = Double.parseDouble(df.format(n));
+        return n;
+    }
+
+}
